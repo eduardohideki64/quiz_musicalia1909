d89da8e6ff9f7297f71a5c09c48acb1c7e00a810
Manuela-Alvees <manuelasenai03@gmail.com>
2024-10-24 14:30:07 -0300

    atuaização

--- musicalia1408/quiz_musicalia0708/src/app/tab1/tab1.page.ts
+++ musicalia1408/quiz_musicalia0708/src/app/tab1/tab1.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public alertButtons = [
+    {
+      text: 'Enviar',
+      handler: (data: { nome?: string; apelido?: string; idade?: number; experiencia?: string }) => {
+        this.handleFormSubmit(data);
+      },
+    }
+  ];
 
+  public alertInputs = [
+    {
+      placeholder: 'Nome',
+      name: 'nome',
+    },
+    {
+      placeholder: 'Apelido',
+      name: 'apelido',
+      attributes: {
+        maxlength: 20,
+      },
+    },
+    {
+      type: 'number',
+      placeholder: 'Idade',
+      name: 'idade',
+      min: 1,
+      max: 100,
+    },
+    {
+      type: 'textarea',
+      placeholder: 'Diga um pouco sobre o sua experiencia com o App:',
+      name: 'experiencia',
+    },
+  ];
+
+  submittedData: any;
+
+  handleFormSubmit(data: { nome?: string; apelido?: string; idade?: number; experiencia?: string }) {
+    this.submittedData = data; // Armazena os dados dos inputs
+  }
 }
