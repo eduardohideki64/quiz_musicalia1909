5ff5b753dfe8b8bc13abe5ce3a881b34ff500620
Manuela-Alvees <manuelasenai03@gmail.com>
2024-10-30 13:54:18 -0300

    atualização
    sorteador de pages ja esta funciuonando, apenas precisa ser dividido entre generos musicais, assim, faltando apenas o conteudo dos demais quizes

--- musicalia1408/quiz_musicalia0708/src/app/musicdodia/musicdodia.page.ts
+++ musicalia1408/quiz_musicalia0708/src/app/musicdodia/musicdodia.page.ts
@@ -5,15 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './musicdodia.page.html',
   styleUrls: ['./musicdodia.page.scss'],
 })
-export class MusicdodiaPage implements OnInit {
+export class MusicdodiaPage{
 
-  constructor() { }
+  frases: string[] = [
+    "A persistência é o caminho do êxito.",
+    "A coragem não é ausência do medo; é a persistência apesar do medo.",
+    "Vencer não é competir com o outro. É derrotar seus inimigos interiores.",
+    "Acredite em você e no seu potencial.",
+    "O sucesso é a soma de pequenos esforços repetidos dia após dia."
+  ];
 
-  ngOnInit() {
+  fraseSorteada: string = '';
+
+  constructor() {}
+
+  sortearFrase() {
+    const indice = Math.floor(Math.random() * this.frases.length);
+    this.fraseSorteada = this.frases[indice];
   }
 
 
 
+  
+  sortearFrase2() {
+    const indice = Math.floor(Math.random() * this.frases.length);
+    this.fraseSorteada = this.frases[indice];
+  }
+  sortearFrase3() {
+    const indice = Math.floor(Math.random() * this.frases.length);
+    this.fraseSorteada = this.frases[indice];
+  }
+  sortearFrase4() {
+    const indice = Math.floor(Math.random() * this.frases.length);
+    this.fraseSorteada = this.frases[indice];
+  }
 
   
 
